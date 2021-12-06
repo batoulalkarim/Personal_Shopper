ec424ad6c8b0bf5973df79ffbedda39a852a71b6
Batoul Alkarim <batoulalkarim@Batouls-MBP.lan>
2021-12-05 19:20:56 -0500

    Getter and Setter Methods Added

---
+++ personal_Shopper.js
@@ -0,0 +1,32 @@
+const outfit = {
+    _pieces: {
+        tops: [],
+        bottoms: [],
+        accessories: [],
+    },
+    get tops() {
+        return this._pieces.tops;
+    },
+    get bottoms() {
+        return this._pieces.bottoms;
+    },
+    get accessories() {
+        return this._pieces.accessories;
+    },
+    set tops(tops) {
+        this._pieces.tops = tops;
+    },
+    set bottoms(bottoms) {
+        this._pieces.bottoms = bottoms;
+    },
+    set accessories(accessories) {
+        this._pieces.accessories = accessories;
+    },
+    get pieces() {
+        return {
+            tops: this.tops,
+            bottoms: this.bottoms,
+            accessories: this.accessories,
+        };
+    },
+}
